b25e7b35751c798640e492f58b943c2706693e57
rezendeUbuntu <g@brielrezen.de>
2022-09-05 22:21:57 -0300

    looping test

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const faceUpCard = document.querySelector("#face-up-card")
 
 const cardGoingOut = () => {
   faceUpCard.style.animationPlayState = "running"
+  setTimeout(() => faceUpCard.style.animationPlayState = "paused", 2000)
 }
 
 faceUpCard.addEventListener("click", cardGoingOut)
