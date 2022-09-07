39d240660c76796dd6132d31832cb1dfbb71978b
rezendeUbuntu <g@brielrezen.de>
2022-09-07 18:31:07 -0300

    always moving on click and hidden scrollbar

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const faceUpCard = document.querySelector("#face-up-card")
 
 const cardGoingOut = () => {
-  faceUpCard.style.animationPlayState = "running"
-  setTimeout(() => faceUpCard.style.animationPlayState = "paused", 2000)
+  faceUpCard.style.animationDuration = "2s"
+  faceUpCard.style.animationName = "going-out"
+  setTimeout(() => {
+    faceUpCard.style.animationName = "" 
+  }, 1000)
 }
 
 faceUpCard.addEventListener("click", cardGoingOut)
