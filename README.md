83468eba1efa45206b8eaae304bc9a4c420eb95b
Gabriel Rezende <g@brielrezen.de>
2022-09-08 20:57:07 -0300

    Loading from file, CSSs /Layout improvements and Readme file

---
+++ README.md
@@ -0,0 +1,12 @@
+# CustomCards
+
+Only reads a text file, line by line and show on cards.
+
+---
+# TODOs
+
+- Fix final card animation
+- Groups, indicate on file groups/levels of phrases
+- Allow configs on file (ex: CONFIG: ordered cardColor(150, 150, 150), borderColor(white) bgColor(150, 150, 100), textColor(200, 200, 200) ORIGINALCASE LOOP)
+- Groups, indicate on file groups/levels of phrases
+- Better CSS for desktop and mobile
--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-const faceUpCard = document.querySelector("#face-up-card")
+const random = size => Math.floor(Math.random() * size)
+
+let phrases = []
+
+let cardsPhrases = []
 
 const cardGoingOut = () => {
   faceUpCard.style.animationDuration = "2s"
   faceUpCard.style.animationName = "going-out"
   setTimeout(() => {
+    faceUpCardText.textContent = faceDownCardText.textContent
+    faceDownCardText.textContent = cardsPhrases.pop() || faceDownCard.remove();
     faceUpCard.style.animationName = "" 
-  }, 1000)
+    }, 1800)
+}
+const faceUpCard = document.querySelector("#face-up-card")
+const faceDownCard = document.querySelector("#face-down-card")
+const faceUpCardText = document.querySelector("#face-up-card-text")
+const faceDownCardText = document.querySelector("#face-down-card-text")
+
+const pickAndRemove = () => {
+  loadedPhrases = phrases
+  for (let i = loadedPhrases.length; i == loadedPhrases.length; i--) {
+    randomItem = random(loadedPhrases.length)
+    cardsPhrases.push(loadedPhrases[randomItem])
+    delete loadedPhrases[randomItem]
+    loadedPhrases = loadedPhrases.filter(el => el !== undefined)
+  }
 }
 
-faceUpCard.addEventListener("click", cardGoingOut)
+const loadCards = () => {
+  pickAndRemove()
+  cardsPhrases = cardsPhrases.slice(0, cardsPhrases.length - 1)
+  
+  faceUpCardText.textContent = cardsPhrases.pop()
+  faceDownCardText.textContent = cardsPhrases.pop()
+  
+  faceUpCard.addEventListener("click", cardGoingOut)
+}
+
+const file = fetch('phrases.txt')
+.then(response => response.text())
+.then(text => {
+  phrases = text.split("\n")
+  loadCards()})
